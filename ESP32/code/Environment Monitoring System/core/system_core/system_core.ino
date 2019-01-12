6299c1f4c8ca8b041406d3f117d17ec647265464
Dreamwalker <aristojeff@gmail.com>
2019-01-12 16:07:08 +0900

    sc

--- ESP32/code/Environment Monitoring System/core/system_core/system_core.ino
+++ ESP32/code/Environment Monitoring System/core/system_core/system_core.ino
@@ -1,9 +1,35 @@
 #include "GravityRtc.h"
-#include "Wire.h"  
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP085_U.h>
 
+Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bmp.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" hPa");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" hPa");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" hPa");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Pressure Sensor Test"); Serial.println("");
 
+  if (!bmp.begin())
+  {
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");  /* There was a problem detecting the BMP085 ... check your connections */
+    while (1);
+  }
+  displaySensorDetails();  /* Display some basic information on this sensor */
 }
 
 void loop() {
