effd3a097abc7b1214d9fd105da0ed7d12ca2ae9
Dreamwalker <aristojeff@gmail.com>
2019-01-12 16:27:00 +0900

    unittest added

--- ESP32/code/Environment Monitoring System/core/system_core/system_core.ino
+++ ESP32/code/Environment Monitoring System/core/system_core/system_core.ino
@@ -1,3 +1,6 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
 #include "GravityRtc.h"
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -44,21 +47,6 @@ void loop() {
   // put your main code here, to run repeatedly:
 
   rtc.read();
-  Serial.print("   Year = ");//year
-  Serial.print(rtc.year);
-  Serial.print("   Month = ");//month
-  Serial.print(rtc.month);
-  Serial.print("   Day = ");//day
-  Serial.print(rtc.day);
-  Serial.print("   Week = ");//week
-  Serial.print(rtc.week);
-  Serial.print("   Hour = ");//hour
-  Serial.print(rtc.hour);
-  Serial.print("   Minute = ");//minute
-  Serial.print(rtc.minute);
-  Serial.print("   Second = ");//second
-  Serial.println(rtc.second);
-
   sensors_event_t event;
   bmp.getEvent(&event);
 
@@ -76,12 +64,42 @@ void loop() {
     Serial.print("Altitude:    ");
     Serial.print(bmp.pressureToAltitude(seaLevelPressure, event.pressure));
     Serial.println(" m");
-    Serial.println("");
+    //    Serial.println("");
   }
   else
   {
-    Serial.println("Sensor error");
+    Serial.println("BMP180 error");
   }
+
+  if (sht30.get() == 0) {
+    Serial.print("Temperature in Celsius : ");
+    Serial.println(sht30.cTemp);
+    Serial.print("Temperature in Fahrenheit : ");
+    Serial.println(sht30.fTemp);
+    Serial.print("Relative Humidity : ");
+    Serial.println(sht30.humidity);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("SHT30 Error!");
+  }
+  
+  Serial.print("Year = ");//year
+  Serial.print(rtc.year);
+  Serial.print("   Month = ");//month
+  Serial.print(rtc.month);
+  Serial.print("   Day = ");//day
+  Serial.print(rtc.day);
+  Serial.print("   Week = ");//week
+  Serial.print(rtc.week);
+  Serial.print("   Hour = ");//hour
+  Serial.print(rtc.hour);
+  Serial.print("   Minute = ");//minute
+  Serial.print(rtc.minute);
+  Serial.print("   Second = ");//second
+  Serial.println(rtc.second);
+  
   delay(1000);
 
 
---
+++ ESP32/code/Environment Monitoring System/unit test/sd_sht30/sd_sht30.ino
@@ -0,0 +1,16 @@
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include <Wire.h>
+#include <WEMOS_SHT3X.h>
+#include "GravityRtc.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
