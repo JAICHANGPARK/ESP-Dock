6d5e6d617c0540d6f5c999a4047d67237c1179b7
Dreamwalker <aristojeff@gmail.com>
2019-01-15 17:57:57 +0900

    macro added

--- ESP32/code/Heart Rate Monitoring/unit test/esp32_dfrobot_hr/esp32_dfrobot_hr.ino
+++ ESP32/code/Heart Rate Monitoring/unit test/esp32_dfrobot_hr/esp32_dfrobot_hr.ino
@@ -1,20 +1,22 @@
 #include <Ticker.h>
 
+#define DF_SON3130
+//#define SEED_HR_EAR
 struct Button {
   const uint8_t PIN;
   uint32_t numberKeyPresses;
   bool pressed;
 };
 
-Button button2 = {12, 0, false};
+Button button2 = {14, 0, false};
 Ticker toggler;
 
 int pushButton = 12;
-volatile uint8_t hrCount = 0;
-boolean hrTickerFlag = false;
+volatile int hrCount = 0;
+volatile boolean hrTickerFlag = false;
 const float togglePeriod = 10; //seconds
 
-
+volatile uint32_t nowTim = 0, lastTim = 0;
 //void IRAM_ATTR isr(void* arg) {
 //  Button* s = static_cast<Button*>(arg);
 //  s->numberKeyPresses += 1;
@@ -22,13 +24,27 @@ const float togglePeriod = 10; //seconds
 //}
 
 void tick() {
-  hrCount = hrCount * 6;
+#ifdef DF_SON3130
+  hrCount = (hrCount * 6) / 2 ;
+#elif SEED_HR_EAR
+  hrCount = (hrCount * 6)  ;
+#endif
+  Serial.print("Heartrate : ");
+  Serial.println(hrCount);
   hrTickerFlag = true;
 }
+
 void IRAM_ATTR isr() {
-  hrCount++;
-  button2.numberKeyPresses += 1;
-  button2.pressed = true;
+  nowTim = millis();
+  uint32_t difTime =  nowTim - lastTim;
+  lastTim = nowTim;
+  if (difTime > 300 || difTime < 2000) {
+    Serial.println("Beat \n");
+    hrCount++;
+  }
+
+  //  button2.numberKeyPresses += 1;
+  //  button2.pressed = true;
 }
 
 // the setup routine runs once when you press reset:
@@ -36,9 +52,9 @@ void setup() {
 
   Serial.begin(9600);
   pinMode(button2.PIN, INPUT_PULLUP);
-  attachInterrupt(button2.PIN, isr, RISING);
-  hrCount = 0;
+  attachInterrupt(button2.PIN, isr, FALLING);
   toggler.attach(togglePeriod, tick);
+  hrCount = 0;
   //  pinMode(pushButton, INPUT);
   //  pinMode(button1.PIN, INPUT_PULLUP);
   //  attachInterruptArg(button1.PIN, isr, &button1, FALLING);
@@ -59,6 +75,7 @@ void loop() {
   if (hrTickerFlag) {
     Serial.println(hrCount);
     hrTickerFlag = false;
+    hrCount = 0;
   }
 
 }
